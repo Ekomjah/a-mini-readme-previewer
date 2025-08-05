58ece952d05f6d589eebd101125b12d3a463fc8f
Ekomjah Denis <ekomjahedet@gmail.com>
2025-08-05 23:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function convertMarkdown() {
   let markdown = `<h${level}>${content}</h${level}>`
   return markdown
 })
+const images = /!\[([^\]]+)\]\(([^)]+)\)/g;
 markdown = markdown.replace(images, `<img src = "$2" alt = "$1"/>`)
 markdown = markdown.replace(/\s{0,3}(\*{2}|_{2})(.*?)\1/g, `<strong>$2</strong>`)
 markdown = markdown.replace(/(\*|_)(.*)\1/g, `<em>$2</em>`)
@@ -24,4 +25,4 @@ markdown = markdown.replace(/^\s{0,3}>\s?(.*)/gm, `<blockquote>$1</blockquote>`)
 markdownInput.addEventListener("input", ()=> convertMarkdown())
 convertMarkdown();
 
- const images = /!\[([^\]]+)\]\(([^)]+)\)/g;
+ 
